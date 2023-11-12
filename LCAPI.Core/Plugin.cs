b48358480c2f6754c3ac08f8f15dcaae0d6a888b
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-11-12 14:40:02 -0600

    Add Styling

--- LCAPI.Core/Plugin.cs
+++ LCAPI.Core/Plugin.cs
@@ -1,14 +1,21 @@
-﻿using BepInEx;
+﻿// -----------------------------------------------------------------------
+// <copyright file="Plugin.cs" company="Lethal Company Modding Community">
+// Copyright (c) Lethal Company Modding Community. All rights reserved.
+// Licensed under the GPL-3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
 
-namespace LCAPI.Core
+namespace LCAPI.Core;
+
+using BepInEx;
+
+/// <inheritdoc />
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
+    private void Awake()
     {
-        private void Awake()
-        {
-            // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
-        }
+        // Plugin startup logic
+        this.Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
     }
 }
