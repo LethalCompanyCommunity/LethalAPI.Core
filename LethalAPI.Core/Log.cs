625f99e120e95f10be5f1ead59222847e33287fd
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-11-14 14:57:12 -0600

    Commit

---
+++ LethalAPI.Core/Log.cs
@@ -0,0 +1,50 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Log.cs" company="LethalAPI Modding Community">
+// Copyright (c) LethalAPI Modding Community. All rights reserved.
+// Licensed under the GPL-3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace LethalAPI.Core;
+
+/// <summary>
+/// The logging class for LethalAPI
+/// </summary>
+public static class Log
+{
+    /// <summary>
+    /// Logs information to the console.
+    /// </summary>
+    /// <param name="message">The message to log.</param>
+    public static void Info(string message)
+    {
+        //
+    }
+
+    /// <summary>
+    /// Logs debugging information to the console.
+    /// </summary>
+    /// <param name="message">The message to log.</param>
+    public static void Debug(string message)
+    {
+        //
+    }
+
+    /// <summary>
+    /// Logs warning information to the console.
+    /// </summary>
+    /// <param name="message">The message to log.</param>
+    public static void Warn(string message)
+    {
+        //
+    }
+
+    /// <summary>
+    /// Logs error information to the console.
+    /// </summary>
+    /// <param name="message">The message to log.</param>
+    public static void Error(string message)
+    {
+        //
+    }
+}
