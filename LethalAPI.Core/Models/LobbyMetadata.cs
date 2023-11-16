9380c216fa867025bb53ef300133f063b787c412
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2023-11-16 16:14:41 +0100

    Add vanilla lobby tags for name & version

--- LethalAPI.Core/Models/LobbyMetadata.cs
+++ LethalAPI.Core/Models/LobbyMetadata.cs
@@ -7,6 +7,7 @@
 
 // ReSharper disable IdentifierTypo
 // ReSharper disable CommentTypo
+
 namespace LethalAPI.Core.Models;
 
 /// <summary>
@@ -15,14 +16,28 @@ namespace LethalAPI.Core.Models;
 public static class LobbyMetadata
 {
     /// <summary>
-    ///     The tag for the lobby being modded.
+    ///     The tag for the lobby name.
     /// </summary>
-    public const string Modded = "__modded";
+    public const string Name = "name";
+
+    /// <summary>
+    ///     The tag for the host's game version.
+    /// </summary>
+    /// <remarks>
+    ///     Users who try to join compare against this. Mismatches cause a failure to join.
+    ///     This should generally not be overridden.
+    /// </remarks>
+    public const string Version = "vers";
 
     /// <summary>
     ///     The tag for the lobby being joinable for vanilla clients.
     /// </summary>
     public const string Joinable = "joinable";
+
+    /// <summary>
+    ///     The tag for the lobby being modded.
+    /// </summary>
+    public const string Modded = "__modded";
 
     /// <summary>
     ///     The tag for the lobby being joinable for modded clients.
