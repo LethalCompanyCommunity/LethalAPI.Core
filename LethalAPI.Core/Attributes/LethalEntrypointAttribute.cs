b26973414fd3b8c57b505f819f7ae3d243003135
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-11-28 12:46:47 -0600

    Update All File Headers.

--- LethalAPI.Core/Attributes/LethalEntrypointAttribute.cs
+++ LethalAPI.Core/Attributes/LethalEntrypointAttribute.cs
@@ -1,7 +1,7 @@
 ﻿// -----------------------------------------------------------------------
 // <copyright file="LethalEntrypointAttribute.cs" company="LethalAPI Modding Community">
 // Copyright (c) LethalAPI Modding Community. All rights reserved.
-// Licensed under the GPL-3.0 license.
+// Licensed under the LGPL-3.0 license.
 // </copyright>
 // -----------------------------------------------------------------------
 
