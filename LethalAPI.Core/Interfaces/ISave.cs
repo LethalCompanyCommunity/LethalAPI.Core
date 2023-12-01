de09340aff6d0eda660e20bc99e9dcbe91740829
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-12-01 16:57:23 -0600

    Add save interfaces for plugin use.

---
+++ LethalAPI.Core/Interfaces/ISave.cs
@@ -0,0 +1,15 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ISave.cs" company="LethalAPI Modding Community">
+// Copyright (c) LethalAPI Modding Community. All rights reserved.
+// Licensed under the GPL-3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace LethalAPI.Core.Interfaces;
+
+/// <summary>
+/// A blank interface that all save instances must inherit.
+/// </summary>
+public interface ISave
+{
+}
---
+++ LethalAPI.Core/Interfaces/ISavePlugin.cs
@@ -0,0 +1,32 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ISaveData.cs" company="LethalAPI Modding Community">
+// Copyright (c) LethalAPI Modding Community. All rights reserved.
+// Licensed under the GPL-3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace LethalAPI.Core.Interfaces;
+
+/// <summary>
+/// A wrapper for inherited plugins to contain a <see cref="ISave"/> property to load and save game data to.
+/// </summary>
+/// <typeparam name="TSave">The class that inherits <see cref="ISave"/> and holds the save data.</typeparam>
+public interface ISavePlugin<TSave> : ISavePlugin
+    where TSave : ISave, new()
+{
+    /// <summary>
+    /// Gets or sets the save data instance.
+    /// </summary>
+    public new TSave SaveData { get; set; }
+}
+
+/// <summary>
+/// Used for internal use for handling saves.
+/// </summary>
+public interface ISavePlugin
+{
+    /// <summary>
+    /// Gets or sets the save data.
+    /// </summary>
+    internal ISave SaveData { get; set; }
+}
