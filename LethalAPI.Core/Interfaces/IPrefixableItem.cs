d25a6a3ba5a72da9fbc31fd88b09b96d40ad3eac
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-12-01 21:59:39 -0600

    Add prefixable item for collection system.

---
+++ LethalAPI.Core/Interfaces/IPrefixableItem.cs
@@ -0,0 +1,19 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IPrefixableItem.cs" company="LethalAPI Modding Community">
+// Copyright (c) LethalAPI Modding Community. All rights reserved.
+// Licensed under the LGPL-3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace LethalAPI.Core.Interfaces;
+
+/// <summary>
+/// Adds a prefix to an item. Used for collections.
+/// </summary>
+public interface IPrefixableItem
+{
+    /// <summary>
+    /// Gets the prefix of the item.
+    /// </summary>
+    string Prefix { get; }
+}
