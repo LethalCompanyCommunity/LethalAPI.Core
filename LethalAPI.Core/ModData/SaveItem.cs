1cd8c82f726eeda4e51f2edae1b47da582253014
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-12-01 22:00:00 -0600

    Add Save Item Collections.

---
+++ LethalAPI.Core/ModData/SaveItem.cs
@@ -0,0 +1,78 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SaveItem.cs" company="LethalAPI Modding Community">
+// Copyright (c) LethalAPI Modding Community. All rights reserved.
+// Licensed under the LGPL-3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+#pragma warning disable SA1402 // File may only contain a single type.
+namespace LethalAPI.Core.ModData;
+
+using Interfaces;
+
+/// <summary>
+/// Represents and item that can be saved.
+/// </summary>
+public class SaveItem : IPrefixableItem
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SaveItem"/> class.
+    /// </summary>
+    /// <param name="prefix">The prefix to store the type as.</param>
+    /// <param name="value">The value of the type.</param>
+    public SaveItem(string prefix, object value)
+    {
+        this.Prefix = prefix;
+        this.Value = value;
+        this.Type = value.GetType();
+    }
+
+    /// <inheritdoc />
+    public string Prefix { get; }
+
+    /// <summary>
+    /// Gets or sets the Value of the item.
+    /// </summary>
+    public object Value { get; set; }
+
+    /// <summary>
+    /// Gets the type of the <see cref="Value"/>.
+    /// </summary>
+    public Type Type { get; }
+}
+
+/// <summary>
+/// A <see cref="SaveItem"/> but as the type.
+/// </summary>
+/// <typeparam name="T">The type of the item.</typeparam>
+public sealed class SaveItem<T> : SaveItem
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SaveItem{T}"/> class.
+    /// </summary>
+    /// <param name="prefix">The prefix to store the type as.</param>
+    /// <param name="value">The value of the type.</param>
+    public SaveItem(string prefix, T value)
+        : base(prefix, value!)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SaveItem{T}"/> class.
+    /// </summary>
+    /// <param name="prefix">The prefix to store the type as.</param>
+    /// <param name="value">The value of the type.</param>
+    public SaveItem(string prefix, object value)
+        : base(prefix, value)
+    {
+    }
+
+    /// <summary>
+    /// Gets or sets the value of the item.
+    /// </summary>
+    public new T Value
+    {
+        get => (T)base.Value;
+        set => base.Value = value!;
+    }
+}
---
+++ LethalAPI.Core/ModData/SaveItemCollection.cs
@@ -0,0 +1,84 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SaveItemCollection.cs" company="LethalAPI Modding Community">
+// Copyright (c) LethalAPI Modding Community. All rights reserved.
+// Licensed under the LGPL-3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace LethalAPI.Core.ModData;
+
+using System.Reflection;
+
+using Features;
+
+/// <summary>
+/// Stores a collection of <see cref="SaveItem">SaveItem's</see>.
+/// </summary>
+internal sealed class SaveItemCollection : Collection<SaveItem>
+{
+    /// <summary>
+    /// Updates an object with the values from this collection.
+    /// </summary>
+    /// <param name="obj">The object to overwrite the values with.</param>
+    internal void UpdateObjectWithValues(ref object obj)
+    {
+        foreach (PropertyInfo property in obj.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+        {
+            if (property.SetMethod is null)
+                continue;
+
+            if (!this.TryGetItem(property.Name, out SaveItem? item))
+                continue;
+
+            if(property.PropertyType != item.Type)
+                continue;
+
+            FieldInfo? backingField = obj.GetType().GetField($"<{property.Name}>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+            if (backingField is null)
+            {
+                Log.Warn($"Could not find backing field for property {property.Name}. This may break things!");
+                property.SetValue(obj, item.Value);
+                continue;
+            }
+
+            backingField.SetValue(obj, item.Value);
+        }
+    }
+
+    /// <summary>
+    /// Updates the collection with the values from an object.
+    /// </summary>
+    /// <param name="obj">The object to read the values from.</param>
+    internal void UpdateCollectionWithObjectValues(ref object obj)
+    {
+        foreach (PropertyInfo property in obj.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+        {
+            if (property.GetMethod is null)
+                continue;
+
+            object value = property.GetValue(obj);
+            if (this.itemsByPrefix.ContainsKey(property.Name))
+            {
+                SaveItem og = this.itemsByPrefix[property.Name];
+                if (og.Type != value.GetType())
+                {
+                    Log.Debug($"Overwriting mod data for item {property.Name}");
+                    og = new SaveItem(og.Prefix, value);
+                }
+                else
+                {
+                    og.Value = value;
+                }
+
+                this.itemsByPrefix[property.Name] = og;
+                int index = this.Items.FindIndex(x => x.Prefix == property.Name);
+                this.Items[index] = og;
+                continue;
+            }
+
+            SaveItem item = new(property.Name, value);
+            this.Items.Add(item);
+            this.itemsByPrefix.Add(property.Name, item);
+        }
+    }
+}
