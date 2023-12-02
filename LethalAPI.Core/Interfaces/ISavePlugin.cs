006fa9c3d97afd8324371d33201162fd06409a94
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-12-01 22:02:15 -0600

    Update Interfaces.

--- LethalAPI.Core/Interfaces/ISavePlugin.cs
+++ LethalAPI.Core/Interfaces/ISavePlugin.cs
@@ -1,32 +1,29 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="ISaveData.cs" company="LethalAPI Modding Community">
+// <copyright file="ISavePlugin.cs" company="LethalAPI Modding Community">
 // Copyright (c) LethalAPI Modding Community. All rights reserved.
-// Licensed under the GPL-3.0 license.
+// Licensed under the LGPL-3.0 license.
 // </copyright>
 // -----------------------------------------------------------------------
 
 namespace LethalAPI.Core.Interfaces;
 
+using ModData.Attributes;
+
 /// <summary>
-/// A wrapper for inherited plugins to contain a <see cref="ISave"/> property to load and save game data to.
+/// A wrapper for <see cref="IPlugin{TConfig}">plugins</see> to contain a <see cref="ISave"/> property to load and save game data to.
 /// </summary>
 /// <typeparam name="TSave">The class that inherits <see cref="ISave"/> and holds the save data.</typeparam>
-public interface ISavePlugin<TSave> : ISavePlugin
+public interface ISavePlugin<TSave>
     where TSave : ISave, new()
 {
     /// <summary>
     /// Gets or sets the save data instance.
     /// </summary>
-    public new TSave SaveData { get; set; }
-}
+    public TSave SaveData { get; set; }
 
-/// <summary>
-/// Used for internal use for handling saves.
-/// </summary>
-public interface ISavePlugin
-{
     /// <summary>
-    /// Gets or sets the save data.
+    /// Gets the settings for the save.
     /// </summary>
-    internal ISave SaveData { get; set; }
+    /// <remarks>This property is get-only because the implementation does not allow for reading the value later.</remarks>
+    public SaveDataAttribute SaveSettings { get; init; }
 }
