1f239005bccd660bf1b62622c5db7b76205bfb86
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-12-02 21:36:17 -0600

    Modify implementation for the save collection

--- LethalAPI.Core/ModData/SaveItemCollection.cs
+++ LethalAPI.Core/ModData/SaveItemCollection.cs
@@ -7,41 +7,55 @@
 
 namespace LethalAPI.Core.ModData;
 
+using System;
+using System.Collections.Generic;
 using System.Reflection;
 
 using Features;
+using Interfaces;
 
 /// <summary>
 /// Stores a collection of <see cref="SaveItem">SaveItem's</see>.
 /// </summary>
-internal sealed class SaveItemCollection : Collection<SaveItem>
+public sealed class SaveItemCollection : Collection<SaveItem>
 {
     /// <summary>
-    /// Updates an object with the values from this collection.
+    /// Initializes a new instance of the <see cref="SaveItemCollection"/> class.
     /// </summary>
-    /// <param name="obj">The object to overwrite the values with.</param>
-    internal void UpdateObjectWithValues(ref object obj)
+    public SaveItemCollection()
     {
-        foreach (PropertyInfo property in obj.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SaveItemCollection"/> class.
+    /// </summary>
+    /// <param name="items">The items to use in the collection.</param>
+    public SaveItemCollection(List<SaveItem> items)
+        : base(items)
+    {
+    }
+
+    /// <summary>
+    /// Gets the collection as a list of saveItems.
+    /// </summary>
+    internal List<SaveItem> AsList => this.Items;
+
+    /// <summary>
+    /// Gets a new save instance from a type, with the values present here.
+    /// </summary>
+    /// <param name="type">The type to make the save from.</param>
+    /// <returns>The newly generated save. Null if an error occured.</returns>
+    internal ISave? GetNewSaveFromValues(Type type)
+    {
+        try
         {
-            if (property.SetMethod is null)
-                continue;
-
-            if (!this.TryGetItem(property.Name, out SaveItem? item))
-                continue;
-
-            if(property.PropertyType != item.Type)
-                continue;
-
-            FieldInfo? backingField = obj.GetType().GetField($"<{property.Name}>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
-            if (backingField is null)
-            {
-                Log.Warn($"Could not find backing field for property {property.Name}. This may break things!");
-                property.SetValue(obj, item.Value);
-                continue;
-            }
-
-            backingField.SetValue(obj, item.Value);
+            object obj = Activator.CreateInstance(type);
+            UpdateObjectWithValues(ref obj);
+            return obj as ISave;
+        }
+        catch (Exception)
+        {
+            return null;
         }
     }
 
@@ -49,7 +63,7 @@ internal sealed class SaveItemCollection : Collection<SaveItem>
     /// Updates the collection with the values from an object.
     /// </summary>
     /// <param name="obj">The object to read the values from.</param>
-    internal void UpdateCollectionWithObjectValues(ref object obj)
+    internal void UpdateCollectionWithObjectValues(object obj)
     {
         foreach (PropertyInfo property in obj.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
         {
@@ -79,6 +93,35 @@ internal sealed class SaveItemCollection : Collection<SaveItem>
             SaveItem item = new(property.Name, value);
             this.Items.Add(item);
             this.itemsByPrefix.Add(property.Name, item);
+        }
+    }
+
+    /// <summary>
+    /// Updates an object with the values from this collection.
+    /// </summary>
+    /// <param name="obj">The object to overwrite the values with.</param>
+    private void UpdateObjectWithValues(ref object obj)
+    {
+        foreach (PropertyInfo property in obj.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+        {
+            if (property.SetMethod is null)
+                continue;
+
+            if (!this.TryGetItem(property.Name, out SaveItem? item))
+                continue;
+
+            if(property.PropertyType != item.Type)
+                continue;
+
+            FieldInfo? backingField = obj.GetType().GetField($"<{property.Name}>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+            if (backingField is null)
+            {
+                Log.Warn($"Could not find backing field for property {property.Name}. This may break things!");
+                property.SetValue(obj, item.Value);
+                continue;
+            }
+
+            backingField.SetValue(obj, item.Value);
         }
     }
 }
