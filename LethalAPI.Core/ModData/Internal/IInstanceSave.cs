9837d5c68cc55bf8aff20e38e0cd1c9e77c499fb
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-12-02 21:34:55 -0600

    Make instance-saving separate from collection saving.

---
+++ LethalAPI.Core/ModData/Internal/IInstanceSave.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IInstanceSave.cs" company="LethalAPI Modding Community">
+// Copyright (c) LethalAPI Modding Community. All rights reserved.
+// Licensed under the LGPL-3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace LethalAPI.Core.ModData.Internal;
+
+using Interfaces;
+
+/// <summary>
+/// Contains features for updating and tracking specific features.
+/// </summary>
+public interface IInstanceSave
+{
+    /// <summary>
+    /// Gets or sets the save instance to use.
+    /// </summary>
+    public abstract ISave Save { get; set; }
+
+    /// <summary>
+    /// Called when a save is updated.
+    /// </summary>
+    public void OnSaveUpdated();
+}
