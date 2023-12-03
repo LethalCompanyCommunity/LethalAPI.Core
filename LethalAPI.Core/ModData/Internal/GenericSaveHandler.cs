1950f55b933e0a5dcc57e991ba5ddfe185e37846
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-12-02 21:33:58 -0600

    Add a generic save handler.

---
+++ LethalAPI.Core/ModData/Internal/GenericSaveHandler.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GenericSaveHandler.cs" company="LethalAPI Modding Community">
+// Copyright (c) LethalAPI Modding Community. All rights reserved.
+// Licensed under the LGPL-3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace LethalAPI.Core.ModData.Internal;
+
+/// <summary>
+/// Contains the implementation for holding a collection of general save data.
+/// </summary>
+public class GenericSaveHandler : SaveHandler
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GenericSaveHandler"/> class.
+    /// </summary>
+    /// <param name="searchGlobal">Whether the save handler is representing a global save or a local save.</param>
+    internal GenericSaveHandler(bool searchGlobal = false)
+    {
+        this.IsGlobalSave = searchGlobal;
+        this.DataCollection = new();
+    }
+}
